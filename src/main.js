92eebd80d34b8f6402546064e90b66f0bb4e2f3a
Bnaya <bnaya.peretz@backslash.security>
2024-02-22 17:19:07 +0200

    nano is a good contender

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
 import * as tw from "twilio";
 import { RequestClient } from "twilio";
+import { } from "@slack/web-api";
+import * as starNano from "nano";
 
 const client = new tw.RequestClient();
 
-client.axios('');
+const serverScope = starNano();
+
+
+function a01() {
+  serverScope.relax();
+}
+
+
+a01();
